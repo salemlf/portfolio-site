185c96cd2b9ddbdb02ae93cfad320eba51dc5f44
salemlf <salemlfenn@gmail.com>
2021-03-07 00:26:16 -0800

    added hamburger animation and overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,12 @@
-console.log("Hello world");
-
 //TODO: change button text and color gradient when switching from light/dark modes
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const header = document.querySelector(".header");
+
+hamburgerBtn.addEventListener("click", function () {
+  console.log("Hamburger open");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+  } else {
+    header.classList.add("open");
+  }
+});
