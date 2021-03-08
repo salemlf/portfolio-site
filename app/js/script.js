076b45218b199a853185c412a2f350d4e3eeb116
salemlf <salemlfenn@gmail.com>
2021-03-07 19:15:16 -0800

    Added slide-in mobile menu, removed gradient code

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,15 @@
 //TODO: change button text and color gradient when switching from light/dark modes
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
 const header = document.querySelector(".header");
+const slideInMenu = document.querySelector(".header__mobile-menu");
 
 hamburgerBtn.addEventListener("click", function () {
   console.log("Hamburger open");
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    slideInMenu.classList.remove("active");
   } else {
     header.classList.add("open");
+    slideInMenu.classList.add("active");
   }
 });
