ac18934c0a773b0aa10113c9d249b43c73db19b1
salemlf <salemlfenn@gmail.com>
2021-06-02 19:43:12 -0700

    Site doesn't scroll when mobile menu is open now

--- app/js/script.js
+++ app/js/script.js
@@ -2,13 +2,16 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
 const header = document.querySelector(".header");
 const slideInMenu = document.querySelector(".header__mobile-menu");
+const documentBody = document.querySelector("body");
 
 hamburgerBtn.addEventListener("click", function () {
   console.log("Hamburger open");
   if (header.classList.contains("open")) {
     header.classList.remove("open");
     slideInMenu.classList.remove("active");
+    document.body.classList.remove("lock-scroll");
   } else {
+    document.body.classList.add("lock-scroll");
     header.classList.add("open");
     slideInMenu.classList.add("active");
   }
